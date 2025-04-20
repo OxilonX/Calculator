a8d0f3830833b3f0b2f058d0c3633dce0af8c4af
oxilon <boabmeuni01@gmail.com>
2025-04-20 08:43:32 +0100

    managed to calculate with keyboard and buttons

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,118 @@
-const result = document.querySelector(".js-result");
-const c = document.querySelector(".js-c");
-const ce = document.querySelector(".js-ce");
-const inv = document.querySelector(".js-inv");
-const plus = document.querySelector(".js-plus");
-const minus = document.querySelector(".js-minus");
-const mult = document.querySelector(".js-mult");
-const div = document.querySelector(".js-div");
-const equal = document.querySelector(".js-equal");
-const dot = document.querySelector(".js-dot");
-const zero = document.querySelector(".js-0");
-const one = document.querySelector(".js-1");
-const two = document.querySelector(".js-2");
-const three = document.querySelector(".js-3");
-const four = document.querySelector(".js-4");
-const five = document.querySelector(".js-5");
-const six = document.querySelector(".js-6");
-const seven = document.querySelector(".js-7");
-const eight = document.querySelector(".js-8");
-const nine = document.querySelector(".js-9");
-const plusMinus = document.querySelector(".js-plus-minus");
-const numbersButtons = document.querySelectorAll(".js-number");
-c;
+document.addEventListener("DOMContentLoaded", function () {
+  const result = document.querySelector(".result");
+  let hiddenResult = "";
+  const numberButtons = document.querySelectorAll(".js-number");
+  const allNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  const operator = document.querySelectorAll(".operator");
+  const equalButton = document.querySelector(".equal-sign");
+  const eraseAllButton = document.querySelector(".js-c");
+  const inverseButton = document.querySelector(".js-inv");
+  /*Functions*/
+
+  function displayNumbers(button) {
+    button.addEventListener("click", function () {
+      result.value += button.textContent;
+    });
+  }
+  function showHiddenResult() {
+    document.querySelector(".js-calcs").innerText = hiddenResult;
+  }
+
+  function arithmeticOperaitions() {
+    document.addEventListener("keydown", (event) => {
+      if (["+", "/", "*", "-"].includes(event.key)) {
+        if (
+          result.value &&
+          !["+", "/", "*", "-"].includes(
+            result.value.charAt(result.value.length - 1)
+          )
+        ) {
+          hiddenResult += result.value + event.key;
+          result.value = "";
+        }
+      }
+      showHiddenResult();
+    });
+  }
+
+  function handleOperator(operator) {
+    if (
+      result.value &&
+      !["+", "/", "*", "-"].includes(result.value.slice(-1))
+    ) {
+      hiddenResult += result.value + operator;
+      result.value = "";
+      showHiddenResult();
+    }
+  }
+
+  function arithmeticOperationsButtons(button) {
+    button.addEventListener("click", () => {
+      handleOperator(button.textContent);
+    });
+  }
+  function keyPressedByUser() {
+    document.addEventListener("keydown", (event) => {
+      if (event.key === "=" || event.key === "Enter") {
+        hiddenResult += result.value;
+        if (
+          eval(hiddenResult) === Infinity ||
+          eval(hiddenResult) === -Infinity
+        ) {
+          result.value = "error";
+          hiddenResult = "";
+        } else {
+          result.value = eval(hiddenResult);
+          hiddenResult = "";
+        }
+      } else if (event.key === "i") {
+        hiddenResult = "";
+        parseFloat(result.value);
+        if (result.value === "0") {
+          result.value = "error";
+        } else {
+          result.value = (1 / result.value).toString();
+        }
+      } else if (allNumbers.includes(event.key)) {
+        result.value += event.key;
+      } else if (event.key === "Backspace") {
+        result.value = result.value.slice(0, -1);
+      } else if (event.key === "c") {
+        result.value = "";
+        hiddenResult = "";
+      }
+    });
+  }
+  function buttonsOnThePage(button1, button2, button3) {
+    button1.addEventListener("click", () => {
+      hiddenResult += result.value;
+      if (eval(hiddenResult) === Infinity || eval(hiddenResult) === -Infinity) {
+        result.value = "error";
+        hiddenResult = "";
+      } else {
+        result.value = eval(hiddenResult);
+        hiddenResult = "";
+      }
+    });
+    button2.addEventListener("click", () => {
+      result.value = "";
+      hiddenResult = "";
+    });
+    button3.addEventListener("click", () => {
+      hiddenResult = "";
+      parseFloat(result.value);
+      if (result.value === "0") {
+        result.value = "error";
+      } else {
+        result.value = (1 / result.value).toString();
+      }
+    });
+  }
+
+  numberButtons.forEach(displayNumbers);
+  keyDisplayNumbers();
+  operator.forEach(arithmeticOperationsButtons);
+  arithmeticOperaitions();
+  buttonsOnThePage(equalButton, eraseAllButton, inverseButton);
+  keyPressedByUser();
+});
